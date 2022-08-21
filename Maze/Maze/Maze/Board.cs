9f002ad86d85d9bbd59a99516ef700d485be430e
Tloz <sksmsdldjeh123@gmail.com>
2022-08-21 23:50:10 +0900

    미로 초기화

---
+++ Maze/Maze/Maze/Board.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Maze
+{
+    class Board
+    {
+
+        public void Initialize()
+        {
+
+        }
+    }
+}
--- Maze/Maze/Maze/Program.cs
+++ Maze/Maze/Maze/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.CursorVisible = false;
+
+        const int WAIT_TICK = 1000 / 30;
+        const char CIRCLE = '\u25cf';
+
+        int lastTick = 0;
+        while (true)
+        {
+            #region 프레임 관리
+            int currentTick = System.Environment.TickCount;
+            if (currentTick - lastTick < WAIT_TICK)
+            {
+                continue;
+            }
+            lastTick = currentTick;
+            #endregion
+
+            Console.SetCursorPosition(0, 0);
+
+            for (int i = 0; i < 25 ; i++)
+            {
+                for (int j = 0; j < 25 ; j++)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.Write(CIRCLE);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
